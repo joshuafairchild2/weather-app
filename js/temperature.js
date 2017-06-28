411712c2d32e5cc6e8cd3521e21f38786bba63ff
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-06-27 23:58:23 -0700

    add a Temperature method to convert kelvin to celsius, and add front end logic to display the converted temperature

--- js/temperature.js
+++ js/temperature.js
@@ -4,6 +4,10 @@ function Temperature() {
 
 Temperature.prototype.kelvinToFahrenheit = function(kelvinDegrees) {
   return ((kelvinDegrees * (9/5)) - 459.67).toFixed(0);
-}
+};
+
+Temperature.prototype.kelvinToCelsius = function(kelvinDegrees) {
+  return (kelvinDegrees - 273.15).toFixed(0);
+};
 
 exports.temperatureModule = Temperature;
--- js/weather.js
+++ js/weather.js
@@ -5,7 +5,7 @@ const tempConverter = new Temperature();
 function Weather() {
 }
 
-Weather.prototype.displayWeather = function(city, displayHumidity, displayKelvin, displayWindSpeed, displayFahrenheit) {
+Weather.prototype.displayWeather = function(city, displayHumidity, displayKelvin, displayWindSpeed, displayFahrenheit, displayCelsius) {
   $.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
     .then(response => displayHumidity(city, response.main.humidity))
     .fail(error => $("#humidity").text(error.responseJSON.message));
@@ -14,15 +14,16 @@ Weather.prototype.displayWeather = function(city, displayHumidity, displayKelvin
     .then(response => {
       const kelvinDegrees = response.main.temp;
       const fahrenheitDegrees = tempConverter.kelvinToFahrenheit(kelvinDegrees);
+      const celsiusDegrees = tempConverter.kelvinToCelsius(kelvinDegrees);
       displayKelvin(city, kelvinDegrees);
       displayFahrenheit(city, fahrenheitDegrees);
+      displayCelsius(city, celsiusDegrees);
     })
     .fail(error => $("#kelvin").text(error.responseJSON.message));
 
   $.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
     .then(response => displayWindSpeed(city, response.wind.speed))
     .fail(error => $("#windspeed").text(error.responseJSON.message));
-
 };
 
 exports.weatherModule = Weather;

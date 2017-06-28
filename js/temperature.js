a5005cef570b23d51a42db0500229cf13491ce1a
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-06-28 00:12:25 -0700

    convert Temperature methods to use implicit return

--- js/temperature.js
+++ js/temperature.js
@@ -2,12 +2,8 @@ function Temperature() {
 
 }
 
-Temperature.prototype.kelvinToFahrenheit = function(kelvinDegrees) {
-  return ((kelvinDegrees * (9/5)) - 459.67).toFixed(0);
-};
+Temperature.prototype.kelvinToFahrenheit = kelvinDegrees => ((kelvinDegrees * (9/5)) - 459.67).toFixed(0);
 
-Temperature.prototype.kelvinToCelsius = function(kelvinDegrees) {
-  return (kelvinDegrees - 273.15).toFixed(0);
-};
+Temperature.prototype.kelvinToCelsius = kelvinDegrees => (kelvinDegrees - 273.15).toFixed(0);
 
 exports.temperatureModule = Temperature;
